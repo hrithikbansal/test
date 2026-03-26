fe67299e3531e8c901e0e4e7be51a5cf15abc897
hrithikbansal <thehrithikbansal@gmail.com>
2026-03-26 15:12:57 -0400

    initial code for pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+
+def main():
+    n= int(input("Enter pyramid dimension: "))
+    for i in range(0,n):
+        for j in range(0,n):
+            if (i+j)>=(n-1):
+                print("#",end="")
+            else:
+                print(" ", end="")
+        print()
+        
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
