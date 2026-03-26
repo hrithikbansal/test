31f65ff1d7fd6f8065524bdb37c4a1b919d41ccf
hrithikbansal <31479430+hrithikbansal@users.noreply.github.com>
2026-03-26 15:54:57 -0400

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -6,10 +6,10 @@ def main():
     n= int(input("Enter pyramid dimension: "))
     for i in range(0,n):
         for j in range(0,n):
-            if (i+j)>=(n-1):
-                print("#",end="")
+            if (i+j)<(n-1):
+                print(" ",end="")
             else:
-                print(" ", end="")
+                print("#", end="")
         print()
         
 
